9a65dcd0a8574d4b404fbe737c6c2e7bd54c55da
davidjkpark <davidjkpark@gmail.com>
2018-04-28 21:15:37 +0900

    180428 commit

--- block.rb
+++ block.rb
@@ -3,13 +3,16 @@ class Blockchain
 	def mining
 		history =[]
 		current_time=Time.now.to_f
-		begin
-			nonce =rand(100000)
-			history<<nonce
-		end while nonce !=0
 
-		history.size
+		begin
+			nonce =rand(1000000)
+			history<<nonce
+			hashed=Digest::SHA256.hexdigest(nonce.to_s)
+		end while hashed[0..3]!='0000'
+
 		Time.now.to_f-current_time
+
+		nonce
 
 	end
 
