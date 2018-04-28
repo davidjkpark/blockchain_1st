dca7a505dae263c8eb062169987f4422fbf06f1d
davidjkpark <davidjkpark@gmail.com>
2018-04-28 20:57:33 +0900

    180428 commit

--- block.rb
+++ block.rb
@@ -1,8 +1,12 @@
 class Blockchain
 
 	def mining
+		history =[]
 		begin
 			nonce =rand(100000)
+			history<<nonce
+			history.push(nonce)
+			history=history+nonce
 		end while nonce !=0
 		nonce
 	end
