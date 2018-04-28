67be49c20f5ef248a3e56768c2f37bda8de12452
davidjkpark <davidjkpark@gmail.com>
2018-04-28 22:22:58 +0900

    180428 커밋

--- server.rb
+++ server.rb
@@ -12,6 +12,9 @@ get '/' do
 
 	aa.all_blocks.each do |b|
 		message<<b['index'].to_s + "<br>"
+		message<<b['time'].to_s + "<br>"
+		message<<b['nonce'].to_s + "<br>"
+		message<<b['previous_address'].to_s + "<br>"
 		message<<"<hr>"
 	end
 
