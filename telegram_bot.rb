ff0a2e29c51607dcc7f8576445c86e93369d6488
davidjkpark <davidjkpark@gmail.com>
2018-05-09 21:51:09 +0900

    180509 commit

---
+++ telegram_bot.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '579503907:AAFPwkuX9j1i7fXH-f4jiB1IL7kUsMtvib0'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "안녕하세요, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
